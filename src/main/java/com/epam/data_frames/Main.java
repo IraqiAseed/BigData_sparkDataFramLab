88d8a69173e53364ca3303f85cedd55b2c9d7fb2
aseed <iraqiaseed@gmail.com>
2021-05-27 19:07:41 +0300

    another solution for find developers with salary < 1200 and which familiar with most popular technology

--- src/main/java/com/epam/data_frames/Main.java
+++ src/main/java/com/epam/data_frames/Main.java
@@ -23,6 +23,8 @@ public class Main {
     public static final String LASTNAME = "lastname";
     public static final String AGE = "age";
     public static final String KEYWORDS = "keywords";
+    public static final String KEYWORD = "keyword";
+
     private static final String SALARY = "salary";
     private static final String NAME = "name";
 
@@ -46,21 +48,27 @@ public class Main {
         Tuple2<String, String>[] types = df.dtypes();
         Arrays.stream(types).collect(Collectors.toList()).forEach(e -> System.out.println(e._1 + "," + e._2) );
 
+
+        //for()
+
+
         //add column salary the value will be calculated by formula: age *numberOf technologies * 10
-        DataFrame DataFrameWithSalary = df.withColumn("salary", col(AGE).cast(DataTypes.IntegerType).multiply(size(col(KEYWORDS))).multiply(10));
+        DataFrame DataFrameWithSalary = df.withColumn(SALARY, col(AGE).multiply(size(col(KEYWORDS))).multiply(10));
         DataFrameWithSalary.show();
 
         //find developers with salary < 1200 and which familiar with most popular technology
-        DataFrame keyWordsExploded = DataFrameWithSalary.withColumn("keyword", explode(col(KEYWORDS))).persist();
+        DataFrame keyWordsExploded = DataFrameWithSalary.withColumn(KEYWORD, explode(col(KEYWORDS))).persist();
 
         DataFrame languageAndFrequency = keyWordsExploded
                 .drop(SALARY).drop(KEYWORDS).drop(NAME).drop(AGE)
-                .groupBy("keyword").count();
+                .groupBy(KEYWORD).count();
 
         languageAndFrequency.show();
         languageAndFrequency.persist();
 
-        Long maxFrequencyValue = languageAndFrequency.agg(max("count")).collectAsList().get(0).getLong(0);
+
+        //sol 1:
+        long maxFrequencyValue = languageAndFrequency.agg(max("count")).collectAsList().get(0).getLong(0);
         DataFrame mostFrequentTechnology = languageAndFrequency.filter(col("count").equalTo(maxFrequencyValue));
         mostFrequentTechnology.show();
         String mostTechnologyFrequent = mostFrequentTechnology.collectAsList().get(0).getString(0);
@@ -68,14 +76,29 @@ public class Main {
         keyWordsExploded.printSchema();
 
         keyWordsExploded = keyWordsExploded
-                .filter(col(SALARY).cast(DataTypes.IntegerType).$less(1200))
+                .filter(col(SALARY).$less(1200))
                 .filter(col("keyword").equalTo(mostTechnologyFrequent));
 
 
         keyWordsExploded.show();
 
+        //sol2:
+        Row row = df.withColumn(KEYWORD, explode(col(KEYWORDS))).select(KEYWORD)
+                .groupBy(col(KEYWORD)).agg(count(KEYWORD).as("amount")).sort(col("amount").desc()).first();
 
+        String mostPopular = row.getAs(KEYWORD);
+        long amount = row.getAs("amount");
 
+        System.out.println(mostPopular);
+
+        DataFrameWithSalary.filter(col(SALARY).leq(1200)
+                .and(array_contains(col(KEYWORDS),mostPopular)))
+                .show();
+
+        DataFrameWithSalary.registerTempTable("employees");
+
+        String sqlText = "select * from employees where salary < 1200";
+        DataFrame dataFrame1 = spark.sql(sqlText);
 
 
         //class exercises
